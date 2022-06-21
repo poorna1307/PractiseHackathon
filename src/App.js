0b7644685b1bde28a33a94087b5a85af40178ec2
TapanGanesh <tapanganesh.naram@gmail.com>
2022-06-21 16:39:19 +0530

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import NavigationBar from './Components/NavigationBar';
-import NavbarSignup from './Components/NavbarSignup';
+import Header from './Components/Header'
 import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
       {/* <NavigationBar/> */}
-      <NavbarSignup/>
-      <Footer/>
-=======
-import Header from './Components/Header';
-function App() {
-  return (
-    <div className="App">
       <Header/>
->>>>>>> dbd9d5a572fbce3e4db01d58e915559b6883e4b3
+      <Footer/>
     </div>
   );
 }
